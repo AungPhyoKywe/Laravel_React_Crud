b678d781b1c45675c2d006afc1cd6f1b9f54b65f
AungPhyoKywe <kophyokywe07@gmail.com>
2020-04-13 16:40:51 +0630

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import styles from './App.module.css';
+import { Line, Bar } from 'react-chartjs-2';
 import 'bootstrap/dist/css/bootstrap.css';
+import image from './images/image.png';
+import CountUp from 'react-countup';
 
 
 export default class App extends React.Component
@@ -14,6 +18,7 @@ export default class App extends React.Component
     deaths:0,
     recovered:0,
     results:0,
+    count:'',
     countries:[],
     currentCountry:'',
 
@@ -41,6 +46,10 @@ export default class App extends React.Component
 
   handleChange = (currentCountry) => {
     console.log(currentCountry.target.value)
+
+    this.setState({
+      count:currentCountry.target.value
+    })
     this.setState({ currentCountry : currentCountry.target.value});
     //console.log(this.state.currentCountry);
     axios.get(`https://covid-193.p.rapidapi.com/statistics?country=${currentCountry.target.value}`,{ headers:  {'x-rapidapi-key' : '1ba4788a8dmshad1c43e94c4c80cp12ce7djsn4a047e8ff6fa'}  })
@@ -54,7 +63,8 @@ export default class App extends React.Component
         total:cases.total,
         recovered:cases.recovered,
         deaths:de.total,
-        day:date
+        day:date,
+        
         
         });
     })
@@ -66,22 +76,58 @@ export default class App extends React.Component
   {
 
     return(
-
-      <div style={{backgroundColor : '#242582'}}className="container">
-
-
+      <div className="container">
             
-
-                <div className=" shadow p-3 mb-5 bg-white rounded card">
-                    <div className="card-header"><h2>စုစုပေါင်းကူးစက်ပြန်နှံ့နေသောနိုင်ငံများ</h2></div>
-                      <div className="card bg-light text-black card-body">
+            <div className="row">
+                <div className="col-sm">
+                  <div className=" shadow p-3 mb-5 bg-white rounded card">
+                    <div className="card-header">ကူစက်ခံရသူ</div>
+                    <div className="card bg-warning text-white card-body">
                       
-                      <center><h1>{this.state.results}</h1></center>
-                      </div> 
+                    <h2><CountUp start={0} end={this.state.total} duration={2.75} separator="," /></h2>
+                    </div> 
+                    </div>
                 </div>
-            
 
-            <select onChange={this.handleChange} className="form-control">
+                <div className="col-sm">
+                  <div className="shadow p-3 mb-5 bg-white rounded card">
+                  <div className="card-header">ပြန်လဲသက်သာလာသူ</div>
+                    <div className="card bg-success text-white card-body ">
+                      
+                      <h2><CountUp start={0} end={this.state.recovered} duration={2.75} separator="," /></h2>
+                    </div> 
+                    </div>
+                </div>
+
+                <div className="col-sm">
+                  <div className=" shadow p-3 mb-5 bg-white rounded card">
+                  <div className="card-header">အသစ်ထက်တွေ့မှု</div>
+                    <div className="card bg-primary text-white card-body ">
+                      
+                      <h2><CountUp start={0} end={this.state.new} duration={2.75} separator="," /></h2>
+                    </div> 
+                    </div>
+                </div>
+
+                <div className="col-sm">
+                  <div className=" shadow p-3 mb-5 bg-white rounded card">
+                  <div className="card-header">သေဆုံးသူ</div>
+                    <div className="card bg-danger text-white card-body">
+                      
+                      <h2><CountUp start={0} end={this.state.deaths} duration={2.75} separator="," /></h2>
+                    </div> 
+                    </div>
+                </div>
+
+                
+                
+
+            </div>
+
+            <div className="row">
+              <div className="col-sm">
+              <center>
+            <select onChange={this.handleChange}style={{ width:300 }} className="form-control">
             {
 
               this.state.countries.map(res =>
@@ -90,52 +136,33 @@ export default class App extends React.Component
             }
              
             </select>
-            <br></br>
-            
-            <div className="row">
-                <div className="col-3">
-                  <div className=" shadow p-3 mb-5 bg-white rounded card">
-                    <div className="card-header">ကူစက်ခံရသူ</div>
-                    <div className="card bg-warning text-white card-body">
-                      
-                      <h2>{this.state.total}</h2>
-                    </div> 
-                    </div>
-                </div>
+            </center>
+              </div>
 
-                <div className="col-3">
-                  <div className="shadow p-3 mb-5 bg-white rounded card">
-                  <div className="card-header">ပြန်လဲသက်သာလာသူ</div>
-                    <div className="card bg-success text-white card-body ">
-                      
-                      <h2>{this.state.recovered}</h2>
-                    </div> 
-                    </div>
-                </div>
+              <div className="col-sm">
+              <img className={styles.image} src={image} alt="COVID-19" />
 
-                <div className="col-3">
-                  <div className=" shadow p-3 mb-5 bg-white rounded card">
-                  <div className="card-header">အသစ်ထက်တွေ့မှု</div>
-                    <div className="card bg-primary text-white card-body ">
-                      
-                      <h2>{this.state.new}</h2>
-                    </div> 
-                    </div>
-                </div>
-
-                <div className="col-3">
-                  <div className=" shadow p-3 mb-5 bg-white rounded card">
-                  <div className="card-header">သေဆုံးသူ</div>
-                    <div className="card bg-danger text-white card-body">
-                      
-                      <h2>{this.state.deaths}</h2>
-                    </div> 
-                    </div>
-                </div>
-                
-
+              </div>
             </div>
-            
+            <Bar
+        data={{
+          labels: ['ကူစက်ခံရသူ', 'ပြန်လဲသက်သာလာသူ', 'သေဆုံးသူ'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [this.state.total, this.state.recovered,this.state.deaths],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${this.state.count}` },
+        }}
+      />
+      
+
+        <center><p><em><i>develop by apk</i></em></p></center>
 
       </div>
        
